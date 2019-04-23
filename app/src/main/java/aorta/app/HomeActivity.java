f67ed2df2da015632348dd746bba24b0624e41ae
jacobvde <40336554+jacobvde@users.noreply.github.com>
2019-04-23 12:01:19 +0200

    Poging 2

--- app/src/main/java/aorta/app/HomeActivity.java
+++ app/src/main/java/aorta/app/HomeActivity.java
@@ -4,25 +4,25 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
 
-import aorta.barcode.BarcodeCaptureActivity;
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class HomeActivity extends AppCompatActivity {
     Button button_logout;
-    Button button_scan_location;
-    TextView mResultTextView;
-    private static final int BARCODE_READER_REQUEST_CODE = 1;
+    Button button_locatie;
+
+    public final int CUSTOMIZED_REQUEST_CODE = 0x0000ffff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        mResultTextView = findViewById(R.id.result_view);
 
         button_logout = (Button) findViewById(R.id.button_logout);
         button_logout.setOnClickListener(new View.OnClickListener() {
@@ -36,17 +36,22 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        button_scan_location = (Button) findViewById(R.id.button_locatie);
-        button_scan_location.setOnClickListener(new View.OnClickListener() {
+        button_locatie = (Button) findViewById(R.id.button_locatie);
+        button_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    goToScanActivity();
+                    scanBarcode(v);
                 } catch (Exception e) {
 
                 }
             }
         });
+
+    }
+
+    public void scanBarcode(View view) {
+        new IntentIntegrator(this).initiateScan();
     }
 
     private void goToMainActivity() {
@@ -54,25 +59,31 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void goToScanActivity() {
-        Intent intent = new Intent(this, BarcodeCaptureActivity.class);
-        startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != CUSTOMIZED_REQUEST_CODE && requestCode != IntentIntegrator.REQUEST_CODE) {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+        switch (requestCode) {
+            case CUSTOMIZED_REQUEST_CODE: {
+                Toast.makeText(this, "REQUEST_CODE = " + requestCode, Toast.LENGTH_LONG).show();
+                break;
+            }
+            default:
+                break;
+        }
+
+        IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
+
+        if(result.getContents() == null) {
+            Log.d("MainActivity", "Cancelled scan");
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("MainActivity", "Scanned");
+            Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+        }
     }
 
-    @Override
-    public  void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == BARCODE_READER_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                if (data != null) {
-                    String barcode = "test";
-                    mResultTextView.setText(barcode);
-                } else
-                    mResultTextView.setText("No barcode captured");
-            }
-            else {
-                Log.e("tag","Het werkt niet! :O");
-            }
-        } else
-            super.onActivityResult(requestCode, resultCode, data);
-    }
 }
